be563e67faa88a64c215dba8e57600c2bb32ad1a
gmsviderski <genrysv@mail.ru>
2024-09-08 22:10:47 +0300

    les2_2

--- BackToFuture/main/views.py
+++ BackToFuture/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Сайт посвящен фильму "Назад в будущее" </h1>')
+    return render(request, 'main/index.html')
 
 def artists(request):
     return HttpResponse('<h1>Артисты, снимамающиеся в фильме </h1>')
