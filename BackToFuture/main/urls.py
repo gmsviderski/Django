c5ebd4dc2cdfbc1dcf8d36291b0e3a3c5ab7bcb5
gmsviderski <genrysv@mail.ru>
2024-09-10 00:03:18 +0300

    temp

--- BackToFuture/main/urls.py
+++ BackToFuture/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('artists', views.artists)
+    path('', views.index, name='home'),
+    path('artists', views.artists, name='personage')
 
 ]
